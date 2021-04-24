80a3b9b1be2f2d38885b1567afc23f30d045bcd6
Pradyoth S P <pradyothsp@Pradyoths-MacBook-Pro.local>
2021-04-24 19:36:29 +0530

    added time

--- telegram_send.py
+++ telegram_send.py
@@ -1,11 +1,11 @@
 import os
+from datetime import datetime
 
 import telebot
 
 from wazirx_get import data
 
-bot = telebot.TeleBot(
-    os.environ.get('TELEGRAM_SECRET'), parse_mode='MARKDOWN')
+bot = telebot.TeleBot(os.environ.get('TELEGRAM_SECRET'), parse_mode='MARKDOWN')
 
 chat_id = os.environ.get('TELEGRAM_CHAT_ID')
 
@@ -15,4 +15,9 @@ def send_message(data):
     return (message.format(data['name'], data['last'], data['low'], data['high'], data['open']))
 
 
-bot.send_message(chat_id, send_message(data))
+try:
+    bot.send_message(chat_id, send_message(data))
+    print("message sent success at {}".format(datetime.now()))
+
+except:
+    print("message failed at: {}".format(datetime.now()))
